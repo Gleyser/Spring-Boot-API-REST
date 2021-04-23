41a552269f6db7face80f629fcc37a362a952c0d
Gleyser Guimarães <gleyser.bonfim.guimaraes@ccc.ufcg.edu.br>
2021-04-22 21:45:10 -0300

    Iniciando o projeto

---
+++ cp/src/test/java/br/com/webproject/cp/cp/CpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.webproject.cp.cp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
