df50874a319e3672eed5b497fc6f03e869a87540
Gleyser Guimarães <gleyser.bonfim.guimaraes@ccc.ufcg.edu.br>
2021-05-15 15:21:35 -0300

    Melhorando o CursoRepository

--- cp/src/main/java/br/com/webproject/cp/cp/repository/CursoRepository.java
+++ cp/src/main/java/br/com/webproject/cp/cp/repository/CursoRepository.java
@@ -1,9 +1,3 @@
 package br.com.webproject.cp.cp.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 
-import br.com.webproject.cp.cp.modelo.Curso;
-
-public interface CursoRepository extends JpaRepository<Curso, Long> {
-
-}
